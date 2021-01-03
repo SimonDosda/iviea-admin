bb90dd88fa30fb9fc83ef7da74dd53e3b4c769cb
Glitch (hello-sqlite) <none>
2021-01-03 12:33:07 +0000

    🏩🍁 Checkpoint ./utils/printful.js:10893996/395

--- utils/printful.js
+++ utils/printful.js
@@ -2,14 +2,28 @@ const fetch = require('node-fetch');
 
 async function getProducts() {
   const res = await fetch("https://api.printful.com/sync/products", {
-        method: "GET",
-       headers: { 
-         "Content-Type": "application/json", 
-         "Authorization": `Basic ${process.env.PRINTFUL_API_KEY}`
-       }
-      });
+    method: "GET",
+    headers: { 
+      "Content-Type": "application/json", 
+      "Authorization": `Basic ${process.env.PRINTFUL_API_KEY}`
+    }
+  });
   const {result} = await res.json();
   return result;
 }
+
+async function getProducWithVariants(productId) {
+  const res = await fetch(`https://api.printful.com/sync/products/${productId}`, {
+    method: "GET",
+    headers: { 
+      "Content-Type": "application/json", 
+      "Authorization": `Basic ${process.env.PRINTFUL_API_KEY}`
+    }
+  });
+  const {result} = await res.json();
+  return result;
+}
+
+async 
 
 module.exports = { getProducts };
