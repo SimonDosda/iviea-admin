0ec6d80a8548793e26b622cb3f427545737b3f07
Glitch (hello-sqlite) <none>
2021-01-05 10:21:37 +0000

    😽🍼 Checkpoint ./db.js:10893996/1993

--- db.js
+++ db.js
@@ -1,6 +1,26 @@
-// init sqlite db
 const fs = require("fs");
-const dbFile = "./.data/sqlite.db";
-const dbExists = fs.existsSync(dbFile);
 const sqlite3 = require("sqlite3").verbose();
+
+const dbFile = "./.data/sqlite.db";
 const db = new sqlite3.Database(dbFile);
+
+if (!fs.existsSync(dbFile)) {
+  initDatabase();
+}
+
+function initDatabase() {
+  db.serialize(async () => {
+    await db.run(
+      "CREATE TABLE Product (id INTEGER PRIMARY KEY AUTOINCREMENT, locale TEXT, name TEXT, sku TEXT);",
+      err => console.log(err)
+    );
+    await db.run(
+      "CREATE TABLE ProductImage (id INTEGER PRIMARY KEY AUTOINCREMENT, product_id INTEGER, src TEXT);",
+      err => console.log(err)
+    );
+    await db.run(
+      "CREATE TABLE Variant (id INTEGER PRIMARY KEY AUTOINCREMENT, product_id INTEGER, name TEXT);",
+      err => console.log(err)
+    );
+  });
+}
